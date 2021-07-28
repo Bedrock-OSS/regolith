e06aca25a3cccfb63da48211ca3528268da9c317
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2021-07-28 16:19:17 -0500

    get the basic CLI lib in place

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module bedrock-oss.github.com/regolith
 
 go 1.16
+
+require github.com/urfave/cli/v2 v2.3.0
--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Regolith | v0.1.0-dev")
+	(&cli.App{
+		Name:  "Regolith",
+		Usage: "A bedrock addon compiler pipeline",
+		Action: func(c *cli.Context) error {
+			//this is where the actual app goes
+			fmt.Printf("Hello %q", c.Args().Get(0))
+			return nil
+		},
+	}).Run(os.Args)
 }
