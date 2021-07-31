835f6851cdcd54f22d541b0c718a237c2cd5e95a
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2021-07-31 11:05:21 -0500

    Add functionality for running a profile

--- src/filters.go
+++ src/filters.go
@@ -1,20 +1,60 @@
 package src
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path"
+
+	"github.com/fatih/color"
+	"github.com/plus3it/gorecurcopy"
 )
 
-func RunProfile(profile string) {
-	log.Println("Configuration:", profile)
+func RunProfile(profileName string) {
+	log.Println("Configuration:", profileName)
 	var project Project = LoadConfig()
-	log.Println(project)
+	//setup directories
+	os.Mkdir("build", 777)
+	os.Mkdir(".tmp", 777)
+	//the first arg specifies the profile of the manifest used
+	profile := project.Profiles[profileName]
+	if profile.Unsafe {
+		log.Print(color.YellowString("Warning! Profile flagged as unsafe. Excercise caution"))
+	}
+	//copy the contents of the `regolith` folder to `.tmp`
+	log.Print(color.BlueString("Copying project files to tempdir"))
+	gorecurcopy.CopyDirectory("regolith", ".tmp")
+	log.Print("done")
+	//now, we go through the filters!
+	for i, filter := range profile.Filters {
+		log.Print(color.CyanString("Running filter %s", filter.Name))
+		filterTmpName := fmt.Sprintf("filter_%d", i)
+		//copy the filter to .tmp for convience(if it's something like python <filter>.py we kinda need it there...
+		datab, _ := ioutil.ReadFile(filter.Location)
+		ioutil.WriteFile(path.Join(".tmp", filterTmpName), datab, 777)
+		//prepend the file to the filter args
+		filter.Arguments = append([]string{filterTmpName}, filter.Arguments...)
+		RunChildProc(filter.RunWith, filter.Arguments, ".tmp")
+		os.Remove(path.Join(".tmp", filterTmpName))
+	}
+	//copy contents of .tmp to build
+	log.Print(color.BlueString("copying .tmp to build"))
+
+	gorecurcopy.CopyDirectory(".tmp", "build")
+	//Done!
+	log.Print(color.GreenString("Finished. built contents -> build/"))
+	//cleanup
+	os.RemoveAll(".tmp")
 }
 
-func RunChildProccess() {
-	cmd := exec.Command("python", "./hello.py")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	log.Println(cmd.Run())
+func RunChildProc(command string, args []string, workingDir string) string {
+	cmd := exec.Command(command, args...)
+	cmd.Dir = workingDir
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out)
 }
