d89a8f8ace7595a97c6f05685de3c5de461675c3
Nusiq <nusiq@outlook.com>
2021-11-04 20:50:35 +0100

    Added test which checks if the file protection gets triggered after adding files to export directory. Close #100

--- test/file_protection_test.go
+++ test/file_protection_test.go
@@ -29,7 +29,7 @@ func TestSwitchingExportTargets(t *testing.T) {
 		t.Fatal("Unable to create temporary directory:", err)
 	}
 	t.Log("Created temporary directory:", tmpDir)
-	// Before "workingDir" the working dir of this test can't be there
+	// Before deleting "workingDir" the test must stop using it
 	defer os.RemoveAll(tmpDir)
 	defer os.Chdir(wd)
 	workingDir := filepath.Join(tmpDir, "working-dir")
@@ -63,5 +63,63 @@ func TestSwitchingExportTargets(t *testing.T) {
 	if err != nil {
 		t.Fatal(
 			"Unable RunProfile failed on second attempt to export to A:", err)
+	}
+}
+
+// TestTriggerFileProtection tests if the file protection system will get
+// triggered when exporting to a target directory with files created not by
+// Regolith. It performs the following:
+// 1. Runs Regolith to export something to a target directory.
+// 2. Creates a file in the target directory.
+// 3. Runs Regolith to export again to the same target directory.
+func TestTriggerFileProtection(t *testing.T) {
+	// Switching working directories in this test, make sure to go back
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Unable to get current working directory")
+	}
+	defer os.Chdir(wd)
+	// Create a temporary directory
+	tmpDir, err := ioutil.TempDir("", "regolith-test")
+	if err != nil {
+		t.Fatal("Unable to create temporary directory:", err)
+	}
+	t.Log("Created temporary directory:", tmpDir)
+	// Before deleting "workingDir" the test must stop using it
+	defer os.RemoveAll(tmpDir)
+	defer os.Chdir(wd)
+	workingDir := filepath.Join(tmpDir, "working-dir")
+	os.Mkdir(workingDir, 0666)
+	// Copy the test project to the working directory
+	err = copy.Copy(
+		multitargetProjectPath,
+		workingDir,
+		copy.Options{PreserveTimes: false, Sync: false},
+	)
+	if err != nil {
+		t.Fatalf(
+			"Failed to copy test files %q into the working directory %q",
+			multitargetProjectPath, workingDir,
+		)
+	}
+	// Switch to the working directory
+	os.Chdir(workingDir)
+	// 1. Run Regolith (export to A)
+	regolith.InitLogging(true)
+	err = regolith.RunProfile("exact_export_A")
+	if err != nil {
+		t.Fatal(
+			"Unable RunProfile failed on first attempt to export to A:", err)
+	}
+	// 2. Create a file in the target directory
+	file, err := os.Create(filepath.Join(tmpDir, "target-a/BP/test-file"))
+	if err != nil {
+		t.Fatal("Unable to create test file:", err)
+	}
+	file.Close()
+	// 3. Run Regolith (export to A)
+	err = regolith.RunProfile("exact_export_A")
+	if err == nil {
+		t.Fatal("Expected RunProfile to fail on second attempt to export to A")
 	}
 }
