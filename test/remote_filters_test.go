512bb10ad669e566be7bee3ea66358c5d6a9de5a
Nusiq <nusiq@outlook.com>
2022-02-19 20:27:19 +0100

    Added tests for 'regolit update' and 'regolith update-all'

--- test/remote_filters_test.go
+++ test/remote_filters_test.go
@@ -10,7 +10,12 @@ import (
 	"github.com/otiai10/copy"
 )
 
-func TestVersionedRemoteFilter(t *testing.T) {
+// TestInstallAllUnlockAndRun runs a project with a modified config.json file.
+// The config file uses a remote filter with a specific version. This test
+// tests 'regolith install-all', 'regolith unlock', and 'regolith run'. The
+// results of running the filter are compared to a project with an expected
+// result.
+func TestInstallAllUnlockAndRun(t *testing.T) {
 	// Switching working directories in this test, make sure to go back
 	wd, err := os.Getwd()
 	if err != nil {
@@ -69,4 +74,144 @@ func TestVersionedRemoteFilter(t *testing.T) {
 	}
 	// Compare the installed dependencies with the expected dependencies
 	comparePathMaps(expectedPaths, createdPaths, t)
+}
+
+// TestInstall tests the 'regolith install' command. It forcefully installs
+// a filter with various versions and compares the outputs with the expected
+// results.
+func TestInstall(t *testing.T) {
+	// SETUP
+	wd, err1 := os.Getwd()
+	defer os.Chdir(wd) // Go back before the test ends
+	tmpDir, err2 := ioutil.TempDir("", "regolith-test")
+	defer os.RemoveAll(tmpDir)
+	defer os.Chdir(wd) // 'tmpDir' can't be used when we delete it
+	err3 := copy.Copy( // Copy the test files
+		freshProjectPath,
+		tmpDir,
+		copy.Options{PreserveTimes: false, Sync: false},
+	)
+	err4 := os.Chdir(tmpDir)
+	if err := firstErr(err1, err2, err3, err4); err != nil {
+		t.Fatalf("Failed to setup test: %v", err)
+	}
+	t.Logf("The testing directory is in: %s", tmpDir)
+
+	// THE TEST
+	filterName := "github.com/Bedrock-OSS/regolith-test-filters/" +
+		"hello-version-python-filter"
+	var regolithInstallProjects = map[string]string{
+		"1.0.0":  "testdata/regolith_install/1.0.0",
+		"1.0.1":  "testdata/regolith_install/1.0.1",
+		"latest": "testdata/regolith_install/latest",
+
+		// The expected result of the HEAD barnch might change in the future
+		// once the test repository is updated. This means that the test data
+		// also needs to be updated.
+		"HEAD": "testdata/regolith_install/HEAD",
+		"0c129227eb90e2f10a038755e4756fdd47e765e6": "testdata/regolith_install/sha",
+		"TEST_TAG_1": "testdata/regolith_install/tag",
+	}
+	for version, expectedResultPath := range regolithInstallProjects {
+		expectedResultPath = filepath.Join(wd, expectedResultPath)
+		// Install the filter with given version
+		err := regolith.Install(
+			[]string{filterName + "==" + version}, true, true)
+		if err != nil {
+			t.Fatal("'regolith install' failed:", err)
+		}
+		// Load expected result
+		expectedPaths, err := listPaths(expectedResultPath, expectedResultPath)
+		if err != nil {
+			t.Fatalf(
+				"Failed to load expected results for version %q: %v",
+				version, err)
+		}
+		// Load created paths for comparison with expected output
+		createdPaths, err := listPaths(".", ".")
+		if err != nil {
+			t.Fatal("Unable to load the created paths:", err)
+		}
+		// Compare the installed dependencies with the expected dependencies
+		comparePathMaps(expectedPaths, createdPaths, t)
+	}
+}
+
+func TestUpdateAndUpdateAll(t *testing.T) {
+	// SETUP
+	wd, err1 := os.Getwd()
+	defer os.Chdir(wd) // Go back before the test ends
+	tmpDir, err2 := ioutil.TempDir("", "regolith-test")
+	defer os.RemoveAll(tmpDir)
+	defer os.Chdir(wd) // 'tmpDir' can't be used when we delete it
+	err3 := copy.Copy( // Copy the test files
+		freshProjectPath,
+		tmpDir,
+		copy.Options{PreserveTimes: false, Sync: false},
+	)
+	err4 := os.Chdir(tmpDir)
+	if err := firstErr(err1, err2, err3, err4); err != nil {
+		t.Fatalf("Failed to setup test: %v", err)
+	}
+	t.Logf("The testing directory is in: %s", tmpDir)
+
+	// THE TEST
+	filterName := "hello-version-python-filter"
+	var regolithInstallProjects = map[string]string{
+		"1.0.0":  "testdata/regolith_install/1.0.0",
+		"1.0.1":  "testdata/regolith_install/1.0.1",
+		"latest": "testdata/regolith_install/latest",
+
+		// The expected result of the HEAD barnch might change in the future
+		// once the test repository is updated. This means that the test data
+		// also needs to be updated.
+		"HEAD": "testdata/regolith_install/HEAD",
+		"0c129227eb90e2f10a038755e4756fdd47e765e6": "testdata/regolith_install/sha",
+		"TEST_TAG_1": "testdata/regolith_install/tag",
+	}
+	updateFunctions := map[string]func([]string) error{
+		"update": func(filters []string) error {
+			return regolith.Update(filters, true)
+		},
+		"update-all": func(_ []string) error {
+			// UpdateAll doesn't use the "filters" argument
+			return regolith.UpdateAll(true)
+		},
+	}
+	for updateFunctionName, updateFunction := range updateFunctions {
+		t.Logf("Testing 'regolith %s'", updateFunctionName)
+		for version, expectedResultPath := range regolithInstallProjects {
+			t.Logf("Testing version %q", version)
+			expectedResultPath = filepath.Join(wd, expectedResultPath)
+			// Copy the config file from expectedResultPath to tmpDir
+			err := copy.Copy(
+				filepath.Join(expectedResultPath, "config.json"),
+				filepath.Join(tmpDir, "config.json"),
+			)
+			if err != nil {
+				t.Fatal("Failed to copy config file for the test setup:", err)
+			}
+			// Run 'regolith update' / 'regolith update-all'
+			err = updateFunction([]string{filterName})
+			if err != nil {
+				t.Fatal("'regolith update' failed:", err)
+			}
+			// Load expected result
+			expectedPaths, err := listPaths(
+				expectedResultPath, expectedResultPath)
+			if err != nil {
+				t.Fatalf(
+					"Failed to load expected results for version %q: %v",
+					version, err)
+			}
+			// Load created paths for comparison with expected output
+			createdPaths, err := listPaths(".", ".")
+			if err != nil {
+				t.Fatal("Unable to load the created paths:", err)
+			}
+			// Compare the installed dependencies with the expected
+			// dependencies
+			comparePathMaps(expectedPaths, createdPaths, t)
+		}
+	}
 }
