77c8bbe77cd8d1e583c39b17ea28e904bdfac08f
Nusiq <nusiq@outlook.com>
2022-02-19 03:06:18 +0100

    Remote filters store their version inside filter.json file when their downloaded (close #74). Added firstErr() utility function for easier handling groups of errors.

--- regolith/filter_remote.go
+++ regolith/filter_remote.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-getter"
@@ -254,10 +255,13 @@ func (i *RemoteFilterDefinition) Download(isForced bool) error {
 	// Can we somehow detect whether this is a failure from git being not
 	// installed, or a failure from
 	// the repo/folder not existing?
-	url, err := i.GetDownloadUrl()
+	repoVersion, err := GetRemoteFilterDownloadRef(
+		i.Url, i.Id, i.Version, true)
 	if err != nil {
-		return WrapError(err, "unable to get download URL")
+		return WrapErrorf(
+			err, "unable to get download URL for filter %q", i.Id)
 	}
+	url := fmt.Sprintf("%s//%s?ref=%s", i.Url, i.Id, repoVersion)
 	downloadPath := i.GetDownloadPath()
 	err = getter.Get(downloadPath, url)
 	if err != nil {
@@ -267,26 +271,41 @@ func (i *RemoteFilterDefinition) Download(isForced bool) error {
 				"	Is git installed?\n"+
 				"	Does that filter exist?", url)
 	}
-
+	// Save the version of the filter we downloaded
+	i.SaveVerssionInfo(repoVersion)
 	// Remove 'test' folder, which we never want to use (saves space on disk)
 	testFolder := path.Join(downloadPath, "test")
 	if _, err := os.Stat(testFolder); err == nil {
 		os.RemoveAll(testFolder)
 	}
 
-	Logger.Infof("Filter %s downloaded successfully.", i.Id)
+	Logger.Infof("Filter %q downloaded successfully.", i.Id)
 	return nil
 }
 
-// GetDownloadUrl creates a download URL, based on the filter definition
-func (i *RemoteFilterDefinition) GetDownloadUrl() (string, error) {
-	repoVersion, err := GetRemoteFilterDownloadRef(
-		i.Url, i.Id, i.Version, true)
-	if err != nil {
-		return "", WrapErrorf(
-			err, "unable to get download URL for filter %q", i.Id)
+// SaveVersionInfo saves puts the specified version string into the
+// filter.json of the remote fileter.
+func (i *RemoteFilterDefinition) SaveVerssionInfo(version string) error {
+	downloadPath := i.GetDownloadPath()
+	filterJsonPath := path.Join(downloadPath, "filter.json")
+	filterJson, err1 := ioutil.ReadFile(filterJsonPath)
+	var filterJsonMap map[string]interface{}
+	err2 := json.Unmarshal(filterJson, &filterJsonMap)
+	if err := firstErr(err1, err2); err != nil {
+		return WrapErrorf(
+			err, "Unable to read \"filter.json\" for %q filter.", i.Id)
 	}
-	return fmt.Sprintf("%s//%s?ref=%s", i.Url, i.Id, repoVersion), nil
+	if strings.HasPrefix(version, i.Id+"-") {
+		version = strings.TrimPrefix(version, i.Id+"-")
+	}
+	filterJsonMap["version"] = version
+	filterJson, _ = json.MarshalIndent(filterJsonMap, "", "\t") // no error
+	err := os.WriteFile(filterJsonPath, filterJson, 0666)
+	if err != nil {
+		return WrapErrorf(
+			err, "Unable to write \"filter.json\" for %q filter.", i.Id)
+	}
+	return nil
 }
 
 // GetDownloadPath returns the path location where the filter can be found.
