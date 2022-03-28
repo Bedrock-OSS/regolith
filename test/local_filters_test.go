693b0c3856951dbf6504b2f05b5cd52419831e7c
Nusiq <nusiq@outlook.com>
2022-03-29 01:58:26 +0200

    Added a new 'exe' type of filter and a test for it. Close #145

--- test/local_filters_test.go
+++ test/local_filters_test.go
@@ -136,3 +136,65 @@ func TestLocalRequirementsInstallAndRun(t *testing.T) {
 		t.Fatal("'regolith run' failed:", err.Error())
 	}
 }
+
+// TextExeFilterRun tests if Regolith can properly run an Exe filter
+func TestExeFilterRun(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Unable to get current working directory")
+	}
+	defer os.Chdir(wd)
+	// Create a temporary directory
+	tmpDir, err := ioutil.TempDir("", "regolith-test")
+	if err != nil {
+		t.Fatal("Unable to create temporary directory:", err)
+	}
+	t.Log("Created temporary directory:", tmpDir)
+	// Before deleting "workingDir" the test must stop using it
+	defer os.RemoveAll(tmpDir)
+	defer os.Chdir(wd)
+	// Copy the test project to the working directory
+	project, err := filepath.Abs(filepath.Join(exeFilterPath, "project"))
+	if err != nil {
+		t.Fatal(
+			"Unable to get absolute path to the test project:", err)
+	}
+	expectedBuildResult, err := filepath.Abs(
+		filepath.Join(exeFilterPath, "expected_build_result"))
+	if err != nil {
+		t.Fatal(
+			"Unable to get absolute path to the expected build result:", err)
+	}
+	err = copy.Copy(
+		project,
+		tmpDir,
+		copy.Options{PreserveTimes: false, Sync: false},
+	)
+	if err != nil {
+		t.Fatalf(
+			"Failed to copy test files from %q into the working directory %q",
+			project, tmpDir,
+		)
+	}
+	// THE TEST
+	os.Chdir(tmpDir)
+	if err := regolith.Unlock(true); err != nil {
+		t.Fatal("'regolith unlock' failed:", err.Error())
+	}
+	if err := regolith.Run("dev", true); err != nil {
+		t.Fatal("'regolith run' failed:", err.Error())
+	}
+	// Load expected result
+	expectedPaths, err := listPaths(expectedBuildResult, expectedBuildResult)
+	if err != nil {
+		t.Fatalf("Failed to load the expected results: %s", err)
+	}
+	// Load actual result
+	tmpDirBuild := filepath.Join(tmpDir, "build")
+	actualPaths, err := listPaths(tmpDirBuild, tmpDirBuild)
+	if err != nil {
+		t.Fatalf("Failed to load the actual results: %s", err)
+	}
+	// Compare the results
+	comparePathMaps(expectedPaths, actualPaths, t)
+}
