c26bbdf502bcf08626ef4f39de864b5825767663
willster277 <willster@willsterjohnson.com>
2022-08-12 23:45:53 +0100

    feat: other os FindMojangDir WSL support

--- regolith/compatibility_other_os.go
+++ regolith/compatibility_other_os.go
@@ -5,6 +5,9 @@ package regolith
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
 )
 
 // venvScriptsPath is a folder name between "venv" and "python" that leads to
@@ -41,7 +44,38 @@ func (d *DirWatcher) Close() error {
 	return fmt.Errorf("Not implemented for this system.")
 }
 
-func FindMojangDir() (string, error) {
-	return "", WrappedErrorf(
-		"Unsupported operating system: '%s'", runtime.GOOS)
+/*
+FindMojangDir locates the com.mojang folder on non-windows platforms.
+
+If the platform is not WSL, this function will return an error.
+
+If WslUser is an empty string, this function will return an error.
+*/
+func FindMojangDir(WslUser string) (string, error) {
+	winUserDir := filepath.Join("/", "mnt", "c", "Users")
+	if _, err := os.Stat(winUserDir); err != nil {
+		return "", WrappedErrorf(
+			"Unsupported operating system: '%s'", runtime.GOOS)
+	}
+
+	if WslUser == "" {
+		return "", WrappedErrorf(
+			"This platform appears to be WSL but the WslUser option was not set.")
+	}
+
+	result := filepath.Join(
+		winUserDir, WslUser, "AppData", "Local", "Packages",
+		"Microsoft.MinecraftUWP_8wekyb3d8bbwe", "LocalState", "games",
+		"com.mojang")
+	if _, err := os.Stat(result); err != nil {
+		if os.IsNotExist(err) {
+			return "", WrapErrorf(
+				err, "The \"com.mojang\" folder is not at \"%s\".\n"+
+					"Does your system have multiple user accounts?", result)
+		}
+		return "", WrapErrorf(
+			err, "Unable to access \"%s\".\n"+
+				"Are your user permissions correct?", result)
+	}
+	return result, nil
 }
