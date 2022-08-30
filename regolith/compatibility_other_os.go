e4d097d4b72c10d8ae17c11c48fca29deef23ae5
Nusiq <nusiq@outlook.com>
2022-08-30 18:41:40 +0200

    Fixed error messages in compatibility_other_os.go

--- regolith/compatibility_other_os.go
+++ regolith/compatibility_other_os.go
@@ -26,30 +26,28 @@ func copyFileSecurityInfo(source string, target string) error {
 type DirWatcher struct{}
 
 func NewDirWatcher(path string) (*DirWatcher, error) {
-	return nil, fmt.Errorf(notImplementedOnThisSystemError)
+	return nil, WrappedError(notImplementedOnThisSystemError)
 }
 
 func (d *DirWatcher) WaitForChange() error {
-	return fmt.Errorf(notImplementedOnThisSystemError)
+	return WrappedError(notImplementedOnThisSystemError)
 }
 
 func (d *DirWatcher) WaitForChangeGroup(
 	groupTimeout uint32, interruptionChannel chan string,
 	interruptionMessage string,
 ) error {
-	return fmt.Errorf(notImplementedOnThisSystemError)
+	return WrappedError(notImplementedOnThisSystemError)
 }
 
 func (d *DirWatcher) Close() error {
-	return fmt.Errorf(notImplementedOnThisSystemError)
+	return WrappedError(notImplementedOnThisSystemError)
 }
 
 func FindMojangDir() (string, error) {
-	return "", WrappedErrorf(
-		"Unsupported operating system: '%s'", runtime.GOOS)
+	return "", WrappedError(notImplementedOnThisSystemError)
 }
 
 func FindPreviewDir() (string, error) {
-	return "", WrappedErrorf(
-		"Unsupported operating system: '%s'", runtime.GOOS)
+	return "", WrappedError(notImplementedOnThisSystemError)
 }
