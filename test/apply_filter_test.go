d7ad4d5f8ac8d4c9f7353f13b38cb9e2dc8eab7b
Nusiq <nusiq@outlook.com>
2023-11-04 20:27:46 +0100

    Refoactored TestApplyFilter test:
    The test files are created inside the project in a .gitignored directory, which
    makes inspecting generated files easier.
    
    Simplified the code.

--- test/apply_filter_test.go
+++ test/apply_filter_test.go
@@ -6,63 +6,29 @@ import (
 	"testing"
 
 	"github.com/Bedrock-OSS/regolith/regolith"
-	"github.com/otiai10/copy"
 )
 
 // TestApplyFilter tests the 'regolith apply-filter' command
 func TestApplyFilter(t *testing.T) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatal("Unable to get current working directory")
-	}
-	defer os.Chdir(wd)
-	// Create a temporary directory
-	tmpDir, err := os.MkdirTemp("", "regolith-test")
-	if err != nil {
-		t.Fatal("Unable to create temporary directory:", err)
-	}
-	t.Log("Created temporary directory:", tmpDir)
-	// Before deleting "workingDir" the test must stop using it
-	defer os.RemoveAll(tmpDir)
-	defer os.Chdir(wd)
-	// Copy the test project to the working directory
-	project, err := filepath.Abs(filepath.Join(applyFilterPath, "project"))
-	if err != nil {
-		t.Fatal(
-			"Unable to get absolute path to the test project:", err)
-	}
-	expectedResult, err := filepath.Abs(
-		filepath.Join(applyFilterPath, "filtered_project"))
-	if err != nil {
-		t.Fatal(
-			"Unable to get absolute path to the expected build result:", err)
-	}
-	err = copy.Copy(
-		project,
-		tmpDir,
-		copy.Options{PreserveTimes: false, Sync: false},
-	)
-	if err != nil {
-		t.Fatalf(
-			"Failed to copy test files from %q into the working directory %q",
-			project, tmpDir,
-		)
-	}
-	// THE TEST
+	// TEST PREPARATION
+	t.Log("Clearing the testing directory...")
+	tmpDir := prepareTestDirectory("TestApplyFilter", t)
+
+	t.Log("Copying the project files into the testing directory...")
+	project := absOrFatal(filepath.Join(applyFilterPath, "project"), t)
+	copyFilesOrFatal(project, tmpDir, t)
+
+	// Load abs path of the expected result and switch to the working directory
+	expectedResult := absOrFatal(
+		filepath.Join(applyFilterPath, "filtered_project"), t)
 	os.Chdir(tmpDir)
+
+	// THE TEST
+	t.Log("Running 'rego apply-filter'...")
 	if err := regolith.ApplyFilter("test_filter", []string{"Regolith"}, true); err != nil {
 		t.Fatal("'regolith apply-filter' failed:", err.Error())
 	}
-	// Load expected result
-	expectedPaths, err := getPathHashes(expectedResult)
-	if err != nil {
-		t.Fatalf("Failed to load the expected results: %s", err)
-	}
-	// Load actual result
-	actualPaths, err := getPathHashes(tmpDir)
-	if err != nil {
-		t.Fatalf("Failed to load the actual results: %s", err)
-	}
-	// Compare the results
-	comparePathMaps(expectedPaths, actualPaths, t)
+	// TEST EVALUATION
+	t.Log("Evaluating the test results...")
+	comparePaths(expectedResult, tmpDir, t)
 }
