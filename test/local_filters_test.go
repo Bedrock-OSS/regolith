3efeeb8e8bf3ff05ca7217914532c6fb8565417f
Nusiq <nusiq@outlook.com>
2023-11-04 15:08:38 +0100

    Refoactored TestExeFilterRun test:
    The test files are created inside the project in a .gitignored directory, which
    makes inspecting generated files easier.
    
    Simplified the code.

--- test/local_filters_test.go
+++ test/local_filters_test.go
@@ -73,61 +73,27 @@ func TestLocalRequirementsInstallAndRun(t *testing.T) {
 
 // TextExeFilterRun tests if Regolith can properly run an Exe filter
 func TestExeFilterRun(t *testing.T) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatal("Unable to get current working directory")
-	}
-	defer os.Chdir(wd)
-	// Create a temporary directory
-	tmpDir, err := os.MkdirTemp("", "regolith-test")
-	if err != nil {
-		t.Fatal("Unable to create temporary directory:", err)
-	}
-	t.Log("Created temporary directory:", tmpDir)
-	// Before deleting "workingDir" the test must stop using it
-	defer os.RemoveAll(tmpDir)
-	defer os.Chdir(wd)
-	// Copy the test project to the working directory
-	project, err := filepath.Abs(filepath.Join(exeFilterPath, "project"))
-	if err != nil {
-		t.Fatal(
-			"Unable to get absolute path to the test project:", err)
-	}
-	expectedBuildResult, err := filepath.Abs(
-		filepath.Join(exeFilterPath, "expected_build_result"))
-	if err != nil {
-		t.Fatal(
-			"Unable to get absolute path to the expected build result:", err)
-	}
-	err = copy.Copy(
-		project,
-		tmpDir,
-		copy.Options{PreserveTimes: false, Sync: false},
-	)
-	if err != nil {
-		t.Fatalf(
-			"Failed to copy test files from %q into the working directory %q",
-			project, tmpDir,
-		)
-	}
-	// THE TEST
+	// TEST PREPARATION
+	t.Log("Clearing the testing directory...")
+	tmpDir := prepareTestDirectory("TestExeFilterRun", t)
+
+	t.Log("Copying the project files into the testing directory...")
+	project := absOrFatal(filepath.Join(exeFilterPath, "project"), t)
+	copyFilesOrFatal(project, tmpDir, t)
+
+	// Load abs path of the expected result and switch to the working directory
+	expectedBuildResult := absOrFatal(
+		filepath.Join(exeFilterPath, "expected_build_result"), t)
 	os.Chdir(tmpDir)
+
+	// THE TEST
+	t.Log("Testing the 'regolith run' command...")
 	if err := regolith.Run("dev", true); err != nil {
 		t.Fatal("'regolith run' failed:", err.Error())
 	}
-	// Load expected result
-	expectedPaths, err := getPathHashes(expectedBuildResult)
-	if err != nil {
-		t.Fatalf("Failed to load the expected results: %s", err)
-	}
-	// Load actual result
-	tmpDirBuild := filepath.Join(tmpDir, "build")
-	actualPaths, err := getPathHashes(tmpDirBuild)
-	if err != nil {
-		t.Fatalf("Failed to load the actual results: %s", err)
-	}
-	// Compare the results
-	comparePathMaps(expectedPaths, actualPaths, t)
+	// TEST EVALUATION
+	t.Log("Evaluating the test results...")
+	comparePaths(expectedBuildResult, filepath.Join(tmpDir, "build"), t)
 }
 
 // TestProfileFilterRun tests valid and invalid profile filters. The invalid
