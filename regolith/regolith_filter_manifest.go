e8ac143322c704202b765323672b637a4f6f1028
Akashic Records <76927522+akashic-records-of-the-abyss@users.noreply.github.com>
2025-02-02 16:13:13 +0000

    Added filter location freedom

--- regolith/regolith_filter_manifest.go
+++ regolith/regolith_filter_manifest.go
@@ -1,6 +1,14 @@
 package regolith
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+
+	"net/http"
+
 	"github.com/Bedrock-OSS/go-burrito/burrito"
 	"golang.org/x/mod/semver"
 )
@@ -25,6 +33,23 @@ type RepositoryManifest struct {
 	Filters       map[string]ManisfestDeclaredFilter `json:"filters,omitempty"`
 }
 
+func (self *RepositoryManifest) FindPath(filterId string) (*string, error) {
+	filter, ok := self.Filters[filterId]
+
+	if !ok {
+		return nil, burrito.WrappedErrorf(`Invalid filter name requested from repository:
+Requested Name: %s`, filterId)
+	}
+
+	out := filter.Path
+
+	if out == nil {
+		return nil, burrito.WrappedErrorf("Requsted path on a filter which does not have a path specified:\nFilter name: %s", filterId)
+	}
+
+	return out, nil
+}
+
 func decodeSingleFilter(obj map[string]interface{}) (*ManisfestDeclaredFilter, error) {
 	result := ManisfestDeclaredFilter{}
 	var path *string = nil
@@ -36,68 +61,67 @@ func decodeSingleFilter(obj map[string]interface{}) (*ManisfestDeclaredFilter, e
 
 	versions, ok := obj["versions"].(map[string]interface{})
 
-	if !ok {
-		return nil, burrito.WrappedErrorf(jsonPropertyMissingError, "versions")
-	}
-
 	result.Path = path
 	result.Versions = make(map[string][]DeclaredFilterInner, len(versions))
 
-	for version, versionObj := range versions {
-		versionInformation, ok := versionObj.(map[string]interface{})
+	if ok {
 
-		if !ok {
-			return nil, burrito.WrappedErrorf(jsonPropertyTypeError, version, "object")
-		}
-
-		if !semver.IsValid("v" + version) {
-			return nil, burrito.WrappedErrorf("Malformed semver. The semver for a filter must be a valid semver. Current: %s", version)
-		}
-
-		urls, ok := versionInformation["urls"].([]map[string]interface{})
-
-		if !ok {
-			return nil, burrito.WrappedErrorf(jsonPropertyMissingError, "urls")
-		}
-
-		for _, inner := range urls {
-			var matcher *OsMatcher = nil
-
-			url, ok := inner["url"].(string)
+		for version, versionObj := range versions {
+			versionInformation, ok := versionObj.(map[string]interface{})
 
 			if !ok {
-				return nil, burrito.WrappedErrorf(jsonPropertyMissingError, "url")
+				return nil, burrito.WrappedErrorf(jsonPropertyTypeError, version, "object")
 			}
 
-			when, ok := inner["when"].(map[string]interface{})
-
-			if ok {
-				var eos *string = nil
-				var earch *string = nil
-
-				os, ok := when["os"].(string)
-
-				if ok {
-					eos = &os
-				}
-
-				arch, ok := when["arch"].(string)
-
-				if ok {
-					earch = &arch
-				}
-
-				matcher = &OsMatcher{
-					ExpectedOs:   eos,
-					ExpectedArch: earch,
-				}
+			if !semver.IsValid("v" + version) {
+				return nil, burrito.WrappedErrorf("Malformed semver. The semver for a filter must be a valid semver. Current: %s", version)
 			}
-			result.Versions[version] = append(result.Versions[version], DeclaredFilterInner{
-				Url:  url,
-				When: matcher,
-			})
+
+			urls, ok := versionInformation["urls"].([]map[string]interface{})
+
+			if !ok {
+				return nil, burrito.WrappedErrorf(jsonPropertyMissingError, "urls")
+			}
+
+			for _, inner := range urls {
+				var matcher *OsMatcher = nil
+
+				url, ok := inner["url"].(string)
+
+				if !ok {
+					return nil, burrito.WrappedErrorf(jsonPropertyMissingError, "url")
+				}
+
+				when, ok := inner["when"].(map[string]interface{})
+
+				if ok {
+					var eos *string = nil
+					var earch *string = nil
+
+					os, ok := when["os"].(string)
+
+					if ok {
+						eos = &os
+					}
+
+					arch, ok := when["arch"].(string)
+
+					if ok {
+						earch = &arch
+					}
+
+					matcher = &OsMatcher{
+						ExpectedOs:   eos,
+						ExpectedArch: earch,
+					}
+				}
+				result.Versions[version] = append(result.Versions[version], DeclaredFilterInner{
+					Url:  url,
+					When: matcher,
+				})
+			}
+
 		}
-
 	}
 
 	return &result, nil
@@ -137,4 +161,58 @@ func RepositoryManifestFromObject(obj map[string]interface{}) (*RepositoryManife
 	}
 
 	return &result, nil
+}
+
+func ManifestForRepo(url string) (*RepositoryManifest, error) {
+	// https://raw.githubusercontent.com/<user-name>/<project-name>/HEAD/regolith_filter_manifest.json is the end result ideally
+	// The url passed in should be in the format github.com/<user-name>/<project-name> so its a trivial transformation
+
+	// Ideally gives us an array which looks like project-name, user-name, Other Stuff We Don't Care About
+	chunks := strings.Split(url, "/")
+
+	if len(chunks) < 2 {
+		return nil, burrito.WrappedErrorf("Manifest url has an invalid format! It should be \"github.com/<user-name>/<project-name>\" it was: %s", url)
+	}
+
+	projectName := chunks[len(chunks)-1]
+	userName := chunks[len(chunks)-2]
+
+	manifestURL := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/HEAD/regolith_filter_manifest.json", userName, projectName)
+
+	Logger.Debugf("Testing URL %s for a manifest", manifestURL)
+
+	var err error
+
+	result, err := http.Get(manifestURL)
+
+	if err != nil {
+		// In this case its fine to fail because the repo might not have a manifest
+		return nil, nil
+	}
+
+	defer result.Body.Close()
+
+	var bytes bytes.Buffer
+
+	_, err = io.Copy(&bytes, result.Body)
+
+	if err != nil {
+		return nil, burrito.WrapErrorf(err, "Failed to clone body of %s manifest", url)
+	}
+
+	object := make(map[string]interface{})
+
+	err = json.Unmarshal(bytes.Bytes(), &object)
+
+	if err != nil {
+		return nil, burrito.WrapErrorf(err, "Failed to decode manifest into json. From the repo: %s", url)
+	}
+
+	manifest, err := RepositoryManifestFromObject(object)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return manifest, nil
 }
--- test/remote_manifest_test.go
+++ test/remote_manifest_test.go
@@ -1,9 +1,30 @@
 package test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/Bedrock-OSS/regolith/regolith"
 )
 
-func someTest(t *testing.T) {
+func TestManifest(t *testing.T) {
+	regolith.InitLogging(true)
+	defer os.Chdir(getWdOrFatal(t))
 
+	t.Log("Clearing the test directory...")
+	tmpDir := prepareTestDirectory("TestRemoteManifest", t)
+	t.Log("Copying the project files into the testing directory...")
+	workingDir := filepath.Join(tmpDir, "working-dir")
+	copyFilesOrFatal(remoteManifestPath, workingDir, t)
+	os.Chdir(workingDir)
+
+	t.Log(tmpDir)
+
+	testUrls := []string{"github.com/akashic-records-of-the-abyss/basic_regolith/nested_filter"}
+
+	err := regolith.Install(testUrls, true, false, false, []string{"default"}, true)
+	if err != nil {
+		t.Fatal("failed to install filter: ", err)
+	}
 }
